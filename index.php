0091d00bc82b0c0810dcf5f963ff650b1034d919
ChristianPucci91 <pucci.christian91@gmail.com>
2021-01-07 16:31:14 +0100

    Esercizio completato

--- index.php
+++ index.php
@@ -19,17 +19,36 @@
        ?>
        <div class="box">
 
-         <h1><?php echo $value['name'] ?></h1>
-         <h3><?php echo $value['description'] ?></h3>
+         <h1><?php echo $value['name']; ?></h1>
+         <h3><?php echo $value['description']; ?></h3>
 
          <ul>
            <!-- se parking = true scrive si altrimenti no -->
-           <li> <strong>Parking:</strong> <?php if ($value['parking']) {
-             echo 'Si';
-           } else {
-             echo 'No';
-           }
-            ?></li>
+           <li>
+             <strong>Parking:</strong>
+             <?php
+               if ($value['parking']) {
+                 echo 'Si';
+               }else {
+                 echo 'No';
+               }
+             ?>
+           </li>
+
+            <li>
+              <strong>Voto: </strong>
+              <?php
+               echo $value['vote'];
+              ?>
+            </li>
+
+            <li>
+              <strong>Distanza dal centro: </strong>
+               <?php
+                 echo $value['distance_to_center'];
+               ?>
+               km
+            </li>
 
          </ul>
 
